e190d60863ef89cc5540fd71c7fb9433753af09f
wesleybizerra <wesleybizerra1@gmail.com>
2026-01-23 13:39:27 -0300

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,22 +1,33 @@
 
+export interface Variation {
+  name: string;
+  price: number;
+}
+
 export interface Product {
   id: string;
   name: string;
-  price: number;
   description: string;
   image: string;
   category: string;
+  variations: Variation[];
 }
 
-export interface CartItem extends Product {
+export interface CartItem {
+  id: string; // ID único: productId_variationName
+  productId: string;
+  name: string;
+  variationName: string;
+  price: number;
+  image: string;
   quantity: number;
 }
 
 export interface CartContextType {
   cart: CartItem[];
-  addToCart: (product: Product) => void;
-  removeFromCart: (productId: string) => void;
-  updateQuantity: (productId: string, delta: number) => void;
+  addToCart: (product: Product, variation: Variation) => void;
+  removeFromCart: (itemId: string) => void;
+  updateQuantity: (itemId: string, delta: number) => void;
   clearCart: () => void;
   totalItems: number;
   totalPrice: number;
